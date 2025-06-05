1e87c1642ddc64467513d594a541d209faee09f0
Eduardo Ferraz <edupferraz@gmail.com>
2025-06-05 13:12:21 -0300

    chore: project setup with architecture, db and docker

---
+++ app-orders/src/db/schema/orders.ts
@@ -0,0 +1,18 @@
+import { timestamp } from "drizzle-orm/pg-core";
+import { integer, pgEnum } from "drizzle-orm/pg-core";
+import { text } from "drizzle-orm/pg-core";
+import { pgTable } from "drizzle-orm/pg-core";
+
+export const orderStatusEnum = pgEnum('order_status', [
+    'pending',
+    'paid',
+    'canceled'
+])
+
+export const orders = pgTable('orders', {
+    id: text().primaryKey(),
+    customerId: text().notNull(),
+    amount: integer().notNull(),
+    status: orderStatusEnum().notNull().default('pending'),
+    createdAt: timestamp().defaultNow().notNull(),
+})
